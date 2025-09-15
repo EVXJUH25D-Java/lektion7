0a5f240d23443c11954eb81d92315ef113a5bb84
EnanderW <williamenander@gmail.com>
2025-09-15 15:13:39 +0200

    add exception classes

---
+++ src/me/code/ProductInsuffientStockException.java
@@ -0,0 +1,9 @@
+package me.code;
+
+public class ProductInsuffientStockException extends Exception {
+
+    public ProductInsuffientStockException(String message) {
+        super(message);
+    }
+
+}
