7a93b33e3bb2d80eb1e97b73eac5aaeb7439d3be
EnanderW <williamenander@gmail.com>
2025-09-15 13:34:44 +0200

    add exception examples

--- src/me/code/Main.java
+++ src/me/code/Main.java
@@ -1,8 +1,35 @@
 package me.code;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Skriv in din ålder: ");
+        String input = scanner.nextLine();
+
+        try {
+            int i = Integer.parseInt(input);
+            System.out.println("Du är " + i + " år gammal.");
+        } catch (Exception exception) {
+            System.out.println("Du måste skriva in ett nummer!");
+            main(args);
+        }
         
+        int index = scanner.nextInt();
+        int value = scanner.nextInt();
+
+        int[] numbers = new int[10];
+        if (index < numbers.length && index >= 0) {
+            numbers[index] = value;
+        } else {
+            System.out.println("Index måste vara mellan 0 och 9");
+        }
+
+        // 1. Exceptions
+        // 2. Try/catch
+        // 3. Validering
+        // 4. Debugging
     }
 }
